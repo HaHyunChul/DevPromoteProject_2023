a060f40da9aaebe256d6e0f279d8cae404e61938
hcha <ggtmuli9@naver.com>
2023-10-04 18:08:31 +0900

    1. Calculator 1 week report source 추가

---
+++ OneNB_DevPromoteProject2013_Calculator_1/Soucre/OneNB_DevPromoteProject2013_Calculator_1/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace OneNB_DevPromoteProject2013_Calculator_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKey7 = new System.Windows.Forms.Button();
+            this.btnKey8 = new System.Windows.Forms.Button();
+            this.btnKey9 = new System.Windows.Forms.Button();
+            this.btnKey4 = new System.Windows.Forms.Button();
+            this.btnKey5 = new System.Windows.Forms.Button();
+            this.btnKey6 = new System.Windows.Forms.Button();
+            this.btnKey1 = new System.Windows.Forms.Button();
+            this.btnKey2 = new System.Windows.Forms.Button();
+            this.btnKey3 = new System.Windows.Forms.Button();
+            this.btnKeySign = new System.Windows.Forms.Button();
+            this.btnKey0 = new System.Windows.Forms.Button();
+            this.btnKeyMinus = new System.Windows.Forms.Button();
+            this.btnKeyPlus = new System.Windows.Forms.Button();
+            this.btnKeyEqual = new System.Windows.Forms.Button();
+            this.btnKeyClear = new System.Windows.Forms.Button();
+            this.btnKeyBackspace = new System.Windows.Forms.Button();
+            this.txtCalcView = new System.Windows.Forms.TextBox();
+            this.txtHistoryView = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnKey7
+            // 
+            this.btnKey7.Location = new System.Drawing.Point(7, 243);
+            this.btnKey7.Name = "btnKey7";
+            this.btnKey7.Size = new System.Drawing.Size(72, 70);
+            this.btnKey7.TabIndex = 0;
+            this.btnKey7.Text = "7";
+            this.btnKey7.UseVisualStyleBackColor = true;
+            this.btnKey7.Click += new System.EventHandler(this.btnKey7_Click);
+            // 
+            // btnKey8
+            // 
+            this.btnKey8.Location = new System.Drawing.Point(85, 243);
+            this.btnKey8.Name = "btnKey8";
+            this.btnKey8.Size = new System.Drawing.Size(72, 70);
+            this.btnKey8.TabIndex = 1;
+            this.btnKey8.Text = "8";
+            this.btnKey8.UseVisualStyleBackColor = true;
+            this.btnKey8.Click += new System.EventHandler(this.btnKey8_Click);
+            // 
+            // btnKey9
+            // 
+            this.btnKey9.Location = new System.Drawing.Point(163, 243);
+            this.btnKey9.Name = "btnKey9";
+            this.btnKey9.Size = new System.Drawing.Size(72, 70);
+            this.btnKey9.TabIndex = 2;
+            this.btnKey9.Text = "9";
+            this.btnKey9.UseVisualStyleBackColor = true;
+            this.btnKey9.Click += new System.EventHandler(this.btnKey9_Click);
+            // 
+            // btnKey4
+            // 
+            this.btnKey4.Location = new System.Drawing.Point(7, 319);
+            this.btnKey4.Name = "btnKey4";
+            this.btnKey4.Size = new System.Drawing.Size(72, 70);
+            this.btnKey4.TabIndex = 3;
+            this.btnKey4.Text = "4";
+            this.btnKey4.UseVisualStyleBackColor = true;
+            this.btnKey4.Click += new System.EventHandler(this.btnKey4_Click);
+            // 
+            // btnKey5
+            // 
+            this.btnKey5.Location = new System.Drawing.Point(85, 319);
+            this.btnKey5.Name = "btnKey5";
+            this.btnKey5.Size = new System.Drawing.Size(72, 70);
+            this.btnKey5.TabIndex = 4;
+            this.btnKey5.Text = "5";
+            this.btnKey5.UseVisualStyleBackColor = true;
+            this.btnKey5.Click += new System.EventHandler(this.btnKey5_Click);
+            // 
+            // btnKey6
+            // 
+            this.btnKey6.Location = new System.Drawing.Point(163, 319);
+            this.btnKey6.Name = "btnKey6";
+            this.btnKey6.Size = new System.Drawing.Size(72, 70);
+            this.btnKey6.TabIndex = 5;
+            this.btnKey6.Text = "6";
+            this.btnKey6.UseVisualStyleBackColor = true;
+            this.btnKey6.Click += new System.EventHandler(this.btnKey6_Click);
+            // 
+            // btnKey1
+            // 
+            this.btnKey1.Location = new System.Drawing.Point(7, 395);
+            this.btnKey1.Name = "btnKey1";
+            this.btnKey1.Size = new System.Drawing.Size(72, 70);
+            this.btnKey1.TabIndex = 6;
+            this.btnKey1.Text = "1";
+            this.btnKey1.UseVisualStyleBackColor = true;
+            this.btnKey1.Click += new System.EventHandler(this.btnKey1_Click);
+            // 
+            // btnKey2
+            // 
+            this.btnKey2.Location = new System.Drawing.Point(85, 395);
+            this.btnKey2.Name = "btnKey2";
+            this.btnKey2.Size = new System.Drawing.Size(72, 70);
+            this.btnKey2.TabIndex = 7;
+            this.btnKey2.Text = "2";
+            this.btnKey2.UseVisualStyleBackColor = true;
+            this.btnKey2.Click += new System.EventHandler(this.btnKey2_Click);
+            // 
+            // btnKey3
+            // 
+            this.btnKey3.Location = new System.Drawing.Point(163, 395);
+            this.btnKey3.Name = "btnKey3";
+            this.btnKey3.Size = new System.Drawing.Size(72, 70);
+            this.btnKey3.TabIndex = 8;
+            this.btnKey3.Text = "3";
+            this.btnKey3.UseVisualStyleBackColor = true;
+            this.btnKey3.Click += new System.EventHandler(this.btnKey3_Click);
+            // 
+            // btnKeySign
+            // 
+            this.btnKeySign.Location = new System.Drawing.Point(7, 471);
+            this.btnKeySign.Name = "btnKeySign";
+            this.btnKeySign.Size = new System.Drawing.Size(72, 70);
+            this.btnKeySign.TabIndex = 9;
+            this.btnKeySign.Text = "+/-";
+            this.btnKeySign.UseVisualStyleBackColor = true;
+            this.btnKeySign.Click += new System.EventHandler(this.btnKeySign_Click);
+            // 
+            // btnKey0
+            // 
+            this.btnKey0.Location = new System.Drawing.Point(85, 471);
+            this.btnKey0.Name = "btnKey0";
+            this.btnKey0.Size = new System.Drawing.Size(72, 70);
+            this.btnKey0.TabIndex = 10;
+            this.btnKey0.Text = "0";
+            this.btnKey0.UseVisualStyleBackColor = true;
+            this.btnKey0.Click += new System.EventHandler(this.btnKey0_Click);
+            // 
+            // btnKeyMinus
+            // 
+            this.btnKeyMinus.Location = new System.Drawing.Point(241, 319);
+            this.btnKeyMinus.Name = "btnKeyMinus";
+            this.btnKeyMinus.Size = new System.Drawing.Size(72, 70);
+            this.btnKeyMinus.TabIndex = 12;
+            this.btnKeyMinus.Text = "-";
+            this.btnKeyMinus.UseVisualStyleBackColor = true;
+            this.btnKeyMinus.Click += new System.EventHandler(this.btnKeyMinus_Click);
+            // 
+            // btnKeyPlus
+            // 
+            this.btnKeyPlus.Location = new System.Drawing.Point(241, 395);
+            this.btnKeyPlus.Name = "btnKeyPlus";
+            this.btnKeyPlus.Size = new System.Drawing.Size(72, 70);
+            this.btnKeyPlus.TabIndex = 13;
+            this.btnKeyPlus.Text = "+";
+            this.btnKeyPlus.UseVisualStyleBackColor = true;
+            this.btnKeyPlus.Click += new System.EventHandler(this.btnKeyPlus_Click);
+            // 
+            // btnKeyEqual
+            // 
+            this.btnKeyEqual.Location = new System.Drawing.Point(241, 471);
+            this.btnKeyEqual.Name = "btnKeyEqual";
+            this.btnKeyEqual.Size = new System.Drawing.Size(72, 70);
+            this.btnKeyEqual.TabIndex = 14;
+            this.btnKeyEqual.Text = "=";
+            this.btnKeyEqual.UseVisualStyleBackColor = true;
+            this.btnKeyEqual.Click += new System.EventHandler(this.btnKeyEqual_Click);
+            // 
+            // btnKeyClear
+            // 
+            this.btnKeyClear.Location = new System.Drawing.Point(163, 167);
+            this.btnKeyClear.Name = "btnKeyClear";
+            this.btnKeyClear.Size = new System.Drawing.Size(72, 70);
+            this.btnKeyClear.TabIndex = 15;
+            this.btnKeyClear.Text = "C";
+            this.btnKeyClear.UseVisualStyleBackColor = true;
+            this.btnKeyClear.Click += new System.EventHandler(this.btnKeyClear_Click);
+            // 
+            // btnKeyBackspace
+            // 
+            this.btnKeyBackspace.Location = new System.Drawing.Point(241, 167);
+            this.btnKeyBackspace.Name = "btnKeyBackspace";
+            this.btnKeyBackspace.Size = new System.Drawing.Size(72, 70);
+            this.btnKeyBackspace.TabIndex = 16;
+            this.btnKeyBackspace.Text = "BC";
+            this.btnKeyBackspace.UseVisualStyleBackColor = true;
+            this.btnKeyBackspace.Click += new System.EventHandler(this.btnKeyBackspace_Click);
+            // 
+            // txtCalcView
+            // 
+            this.txtCalcView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCalcView.Font = new System.Drawing.Font("굴림", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtCalcView.Location = new System.Drawing.Point(7, 91);
+            this.txtCalcView.Name = "txtCalcView";
+            this.txtCalcView.ReadOnly = true;
+            this.txtCalcView.Size = new System.Drawing.Size(306, 46);
+            this.txtCalcView.TabIndex = 17;
+            this.txtCalcView.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtHistoryView
+            // 
+            this.txtHistoryView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtHistoryView.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtHistoryView.Location = new System.Drawing.Point(6, 55);
+            this.txtHistoryView.Name = "txtHistoryView";
+            this.txtHistoryView.ReadOnly = true;
+            this.txtHistoryView.Size = new System.Drawing.Size(306, 23);
+            this.txtHistoryView.TabIndex = 18;
+            this.txtHistoryView.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 548);
+            this.Controls.Add(this.txtHistoryView);
+            this.Controls.Add(this.txtCalcView);
+            this.Controls.Add(this.btnKeyBackspace);
+            this.Controls.Add(this.btnKeyClear);
+            this.Controls.Add(this.btnKeyEqual);
+            this.Controls.Add(this.btnKeyPlus);
+            this.Controls.Add(this.btnKeyMinus);
+            this.Controls.Add(this.btnKey0);
+            this.Controls.Add(this.btnKeySign);
+            this.Controls.Add(this.btnKey3);
+            this.Controls.Add(this.btnKey2);
+            this.Controls.Add(this.btnKey1);
+            this.Controls.Add(this.btnKey6);
+            this.Controls.Add(this.btnKey5);
+            this.Controls.Add(this.btnKey4);
+            this.Controls.Add(this.btnKey9);
+            this.Controls.Add(this.btnKey8);
+            this.Controls.Add(this.btnKey7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKey7;
+        private System.Windows.Forms.Button btnKey8;
+        private System.Windows.Forms.Button btnKey9;
+        private System.Windows.Forms.Button btnKey4;
+        private System.Windows.Forms.Button btnKey5;
+        private System.Windows.Forms.Button btnKey6;
+        private System.Windows.Forms.Button btnKey1;
+        private System.Windows.Forms.Button btnKey2;
+        private System.Windows.Forms.Button btnKey3;
+        private System.Windows.Forms.Button btnKeySign;
+        private System.Windows.Forms.Button btnKey0;
+        private System.Windows.Forms.Button btnKeyMinus;
+        private System.Windows.Forms.Button btnKeyPlus;
+        private System.Windows.Forms.Button btnKeyEqual;
+        private System.Windows.Forms.Button btnKeyClear;
+        private System.Windows.Forms.Button btnKeyBackspace;
+        private System.Windows.Forms.TextBox txtCalcView;
+        private System.Windows.Forms.TextBox txtHistoryView;
+    }
+}
+
---
+++ OneNB_DevPromoteProject2013_Calculator_1/Soucre/OneNB_DevPromoteProject2013_Calculator_1/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneNB_DevPromoteProject2013_Calculator_1
+{
+    enum OPERATOR_MODE
+    {
+        EQUAL,
+        PULS,
+        MINUS,
+        NONE,
+    };
+
+    enum SIGN_MODE
+    {
+        POSITIVE,
+        NEGATIVE,
+    };
+
+    enum KEY_MODE
+    {
+        KEY_0,
+        KEY_1,
+        KEY_2,
+        KEY_3,
+        KEY_4,
+        KEY_5,
+        KEY_6,
+        KEY_7,
+        KEY_8,
+        KEY_9,
+        KEY_SIGN,
+        KEY_CLEAR,
+        KEY_BACKSPACE,        
+        KEY_PULS,
+        KEY_MINUS,
+        KEY_EQUAL,
+        KEY_NONE,        
+    };
+
+    public partial class Form1 : Form
+    {
+        int maxCalcViewNum = int.MaxValue;
+        int minCalcViewNum = int.MinValue;
+
+        string calcView = string.Empty;
+        string historyView = string.Empty;
+
+        KEY_MODE lastKey = KEY_MODE.KEY_0;
+
+        SIGN_MODE[] paramSign = new SIGN_MODE[2] { SIGN_MODE.POSITIVE, SIGN_MODE.POSITIVE };
+        OPERATOR_MODE[] paramOperator = new OPERATOR_MODE[2] { OPERATOR_MODE.NONE, OPERATOR_MODE.NONE };
+
+        int[] paramCalc = new int[2] { 0, 0 };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtCalcView.Text = "0";
+        }
+
+        private void btnKey0_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateCalcView(ref view, false);
+            int.TryParse(view, out calc);
+
+            if (calc != 0)
+            {
+                if (AddCalcView("0") == true)
+                {
+                    lastKey = KEY_MODE.KEY_0;
+                }
+
+            }
+        }
+
+        private void btnKey1_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("1") == true)
+            {
+                lastKey = KEY_MODE.KEY_1;
+            }
+        }
+
+        private void btnKey2_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("2") == true)
+            {
+                lastKey = KEY_MODE.KEY_2;
+            }
+        }
+
+        private void btnKey3_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("3") == true)
+            {
+                lastKey = KEY_MODE.KEY_3;
+            }
+        }
+
+        private void btnKey4_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("4") == true)
+            {
+                lastKey = KEY_MODE.KEY_4;
+            }
+        }
+
+        private void btnKey5_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("5") == true)
+            {
+                lastKey = KEY_MODE.KEY_5;
+            }
+        }
+
+        private void btnKey6_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("6") == true)
+            {
+                lastKey = KEY_MODE.KEY_6;
+            }
+        }
+
+        private void btnKey7_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("7") == true)
+            {
+                lastKey = KEY_MODE.KEY_7;
+            }
+        }
+
+        private void btnKey8_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("8") == true)
+            {
+                lastKey = KEY_MODE.KEY_8;
+            }
+        }
+
+        private void btnKey9_Click(object sender, EventArgs e)
+        {
+            if (AddCalcView("9") == true)
+            {
+                lastKey = KEY_MODE.KEY_9;
+            }
+        }
+
+        private void btnKeySign_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateCalcView(ref view, false);
+            int.TryParse(view, out calc);
+
+            calc *= -1;
+            view = calc.ToString();
+            UpdateCalcView(ref view);
+        }
+
+        private void btnKeyPlus_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateCalcView(ref view, false);
+            int.TryParse(view, out calc);
+
+            if(lastKey != KEY_MODE.KEY_PULS && lastKey != KEY_MODE.KEY_MINUS)
+            {
+                if (paramOperator[0] == OPERATOR_MODE.NONE || paramOperator[0] == OPERATOR_MODE.EQUAL)
+                {
+                    paramCalc[0] = calc;
+                }
+                else
+                {
+                    paramCalc[1] = calc;
+                    calc = Calc(paramCalc[0], paramCalc[1], paramOperator[0]);
+                    paramCalc[0] = calc;
+                }
+            }
+            
+            paramOperator[0] = OPERATOR_MODE.PULS;
+            lastKey = KEY_MODE.KEY_PULS;
+
+            view = calc.ToString();
+            UpdateCalcView(ref view);
+
+            UpdateHistoryView(ref view);
+            AddHistroyView("+");
+        }
+
+        private void btnKeyMinus_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateCalcView(ref view, false);
+            int.TryParse(view, out calc);
+
+            if (lastKey != KEY_MODE.KEY_PULS && lastKey != KEY_MODE.KEY_MINUS)
+            {
+                if (paramOperator[0] == OPERATOR_MODE.NONE || paramOperator[0] == OPERATOR_MODE.EQUAL)
+                {
+                    paramCalc[0] = calc;
+                }
+                else
+                {
+                    paramCalc[1] = calc;
+                    calc = Calc(paramCalc[0], paramCalc[1], paramOperator[0]);
+                    paramCalc[0] = calc;
+                }
+            }
+            paramOperator[0] = OPERATOR_MODE.MINUS;
+            lastKey = KEY_MODE.KEY_MINUS;
+
+            view = calc.ToString();
+            UpdateCalcView(ref view);
+
+            UpdateHistoryView(ref view);
+            AddHistroyView("-");
+        }
+
+        private void btnKeyEqual_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateCalcView(ref view, false);
+            int.TryParse(view, out calc);
+
+            if (lastKey != KEY_MODE.KEY_PULS && lastKey != KEY_MODE.KEY_MINUS)
+            {
+                if (paramOperator[0] == OPERATOR_MODE.NONE || paramOperator[0] == OPERATOR_MODE.EQUAL)
+                {
+                    paramCalc[0] = calc;
+                }
+                else
+                {
+                    paramCalc[1] = calc;
+                    calc = Calc(paramCalc[0], paramCalc[1], paramOperator[0]);
+                    paramCalc[0] = calc;
+                }
+            }
+            paramOperator[0] = OPERATOR_MODE.EQUAL;
+            lastKey = KEY_MODE.KEY_EQUAL;
+
+            view = calc.ToString();
+            UpdateCalcView(ref view);
+
+            UpdateHistoryView(ref view);
+            AddHistroyView("=");
+        }
+
+        private void btnKeyClear_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+            UpdateCalcView(ref view);
+            UpdateHistoryView(ref view);
+
+            paramCalc[0] = 0;
+            paramCalc[1] = 0;
+        
+            paramOperator[0] = OPERATOR_MODE.NONE;
+            paramOperator[1] = OPERATOR_MODE.NONE;        
+            
+            lastKey = KEY_MODE.KEY_CLEAR;
+        }
+
+        private void btnKeyBackspace_Click(object sender, EventArgs e)
+        {
+            string view = string.Empty;
+
+            UpdateCalcView(ref view, false);
+            if (0 < view.Length)
+            {
+                view = view.Remove(view.Length - 1, 1);
+
+                UpdateCalcView(ref view);
+            }
+        }
+
+        private int Calc(int param0, int param1, OPERATOR_MODE operator0)
+        {
+            int result;
+
+            switch(operator0)
+            {
+                case OPERATOR_MODE.PULS:
+                    result = param0 + param1;
+                    break;
+
+                case OPERATOR_MODE.MINUS:
+                    result = param0 - param1;
+                    break;
+
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+
+        private void SetParamCalc(int indexParam)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateCalcView(ref view, false);
+            int.TryParse(view, out calc);
+            
+            paramCalc[indexParam] = calc;
+        }
+
+        private bool AddCalcView(string addCalc)
+        {            
+            string view = string.Empty;
+            int calc;
+
+            
+            UpdateCalcView(ref view, false);
+
+            if (lastKey == KEY_MODE.KEY_PULS || lastKey == KEY_MODE.KEY_MINUS)
+            {
+                view = "0";
+            }
+
+            if (int.TryParse(view + addCalc, out calc) == true)
+            {
+                if (minCalcViewNum <= calc && calc <= maxCalcViewNum)
+                {
+                    if (view == "0")
+                    {
+                        view = addCalc;
+                    }
+                    else
+                    {
+                        view += addCalc;
+                    }
+
+                    UpdateCalcView(ref view);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private void UpdateCalcView(ref string calc, bool update = true)
+        {
+            if(update == true)
+            {                
+                txtCalcView.Text = calc;
+            }
+            else
+            {
+                calc = txtCalcView.Text;
+            }             
+        }
+
+        private bool AddHistroyView(string addHistroy)
+        {
+            string view = string.Empty;
+            int calc;
+
+            UpdateHistoryView(ref view, false);
+            view += addHistroy;
+            UpdateHistoryView(ref view);
+
+            return false;
+        }
+
+
+        private void UpdateHistoryView(ref string history, bool update = true)
+        {            
+            if (update == true)
+            {
+                txtHistoryView.Text = history;
+            }
+            else
+            {
+                history = txtHistoryView.Text;
+            }
+        }
+    }
+}
